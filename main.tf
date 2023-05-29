c964d11e739f2cca73a9d172237fafdc4941c111
Sarwan Kr. Sharma <sarwan.sharma@hotmail.com>
2023-05-29 22:06:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "vsphere" {
-  user                 = root
+  user                 = "root"
   password             = var.password
   vsphere_server       = "vcsa.vsphere.local"
   allow_unverified_ssl = true
